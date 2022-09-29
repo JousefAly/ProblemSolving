6f03df38fd6ce900f6790272207471267e5be60e
YousefAly0 <y.aly@tildetech.ae>
2022-09-29 12:53:49 +0200

    running sum problem

---
+++ LeetCode/LeetCode/Problems/RunningSumProblem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    public class RunningSumProblem
+    {
+        public static int[] RunningSum(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i != 0)
+                    nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;  
+        }
+    }
+}
--- LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/Program.cs
@@ -9,8 +9,19 @@ namespace LeetCode
         {
             while (true)
             {
-                Console.WriteLine("Enter roman number:");
-                Console.WriteLine(Problems0.RomanToInt(Console.ReadLine()));
+                //Console.WriteLine("Enter roman number:");
+                //Console.WriteLine(Problems0.RomanToInt(Console.ReadLine()));
+                Console.WriteLine("array size:");
+                var size = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter array numbers");
+                var nums = new int[size];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    Console.WriteLine("num: ");
+                    nums[i] = int.Parse(Console.ReadLine());
+                }
+                var result = RunningSumProblem.RunningSum(nums);
+                Console.WriteLine(string.Join(',', result));
 
             }
         }
