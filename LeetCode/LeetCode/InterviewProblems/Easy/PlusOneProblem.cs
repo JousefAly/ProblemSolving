3b7d4fb2efccd003934c1a2c54749234932dff09
YousefAly0 <y.aly@tildetech.ae>
2022-10-09 16:27:25 +0200

    added configs

---
+++ LeetCode/LeetCode/InterviewProblems/Easy/PlusOneProblem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.InterviewProblems.Easy
+{
+    public class PlusOneProblem
+    {
+        public int[] PlusOne(int[] digits)
+        {
+            var lastElement = digits[digits.Length - 1]
+        }
+    }
+}
