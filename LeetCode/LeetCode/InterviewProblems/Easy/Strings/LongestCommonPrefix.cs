73dda345be425b64256a5fa42d01c82c9335c38d
YousefAly0 <y.aly@tildetech.ae>
2022-10-17 16:20:49 +0200

    added LongestCommonPrefix problem

---
+++ LeetCode/LeetCode/InterviewProblems/Easy/Strings/LongestCommonPrefix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.InterviewProblems.Easy.Strings
+{
+    public class LongestCommonPrefix
+    {
+        public static string LongestPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return string.Empty;
+            var firstWord = strs[0];
+            var prefix = new StringBuilder("");
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                for (int s = 1; s < strs.Length; s++)
+                {
+
+                    if (i >= strs[s].Length || strs[s][i] != firstWord[i])
+                        return prefix.ToString();
+                }
+                prefix.Append(firstWord[i]);
+            }
+            return prefix.ToString();
+        }
+    }
+}
