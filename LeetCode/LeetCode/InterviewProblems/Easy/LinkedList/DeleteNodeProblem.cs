59d02ad84c34be73604d04d8f683396c3cdb2b06
YousefAly0 <y.aly@tildetech.ae>
2022-10-18 11:14:08 +0200

    added delete node from linked list problem

---
+++ LeetCode/LeetCode/InterviewProblems/Easy/LinkedList/DeleteNodeProblem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.InterviewProblems.Easy.LinkedList
+{
+
+    //Definition for singly-linked list.
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public class DeleteNodeProblem
+    {
+        public static void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
