7133e7a732ceca1350ebf8ca716d41a56998d5b4
Yousef Aly <yousef.aly@capstonelogistics.com>
2023-08-29 15:53:44 +0300

    team

--- MostafaSaadSheet/MostafaSaadSheet/Program.cs
+++ MostafaSaadSheet/MostafaSaadSheet/Program.cs
@@ -4,9 +4,19 @@
 	{
 		static void Main(string[] args)
 		{
-			
+			var n = int.Parse(Console.ReadLine());
 
-		}
+			int totalProblems = 0;
+			for (int i = 0; i < n; i++)
+			{
+				string[] inputLine = Console.ReadLine().Split(' ');
+				if(inputLine.Select(int.Parse).Sum() >= 2)
+					totalProblems++;
+
+			}
+
+            Console.WriteLine(totalProblems);
+        }
 
 	}
 }
