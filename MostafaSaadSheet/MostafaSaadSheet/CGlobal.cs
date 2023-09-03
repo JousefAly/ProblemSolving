2b578af34a3980a0bc279150077c33a5a857c47b
Yousef Aly <yousef.aly@capstonelogistics.com>
2023-09-03 17:50:33 +0300

    problems

---
+++ MostafaSaadSheet/MostafaSaadSheet/CGlobal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostafaSaadSheet
+{
+	public static class CGlobal
+	{
+		public static int[] ReadIntArray()
+		{
+			return Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+		}
+	}
+}
--- MostafaSaadSheet/MostafaSaadSheet/Program.cs
+++ MostafaSaadSheet/MostafaSaadSheet/Program.cs
@@ -4,23 +4,11 @@
 	{
 		static void Main(string[] args)
 		{
-			int row = 0;
-			int col = 0;
-			for (int i = 1; i <= 5; i++)
-			{
-				int[] line = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-				for (int j = 1; j <= 5; j++)
-				{
-					if (line[j - 1] == 1)
-					{
-						row = i;
-						col = j;
-						break;
-					}
-				}
-			}
-			int result = Math.Abs(3 - row) + Math.Abs(3 - col);
-            Console.WriteLine(result);
+			int n = int.Parse(Console.ReadLine());
+
+			int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(string.Join(" ", arr));
         }
 
 	}
